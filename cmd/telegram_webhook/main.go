e5c7d8b4575455ed8f100ae9cedfcc92d04d7af2
TheMysteriousVincent <agpheaddikilla@yahoo.de>
2018-07-02 19:41:58 +0200

    update telegram webhook

--- cmd/telegram_webhook/main.go
+++ cmd/telegram_webhook/main.go
@@ -7,9 +7,8 @@ import (
 
 	"github.com/playnet-public/flagenv"
 
-	"github.com/gorilla/mux"
-
-	"github.com/urfave/negroni"
+//	"github.com/gorilla/mux"
+//	"github.com/urfave/negroni"
 
 	tba "github.com/TheMysteriousVincent/telegram-bot-api"
 )
@@ -21,9 +20,11 @@ const (
 var (
 	apiKey      = flagenv.String("telegram-bot-api-key", "", "This is your telegram bot-api-token")
 	webhookHost = flagenv.String("webhook-host", "localhost", "The webhook host")
-	webhookPort = flagenv.Int("webhook-port", 3030, "The webhook port")
+	webhookPort = flagenv.Int("webhook-port", 88, "The webhook port")
 	host        = flagenv.String("host", "0.0.0.0", "The host of the http server")
-	port        = flagenv.Int("port", 3030, "The port of the http server")
+	port        = flagenv.Int("port", 88, "The port of the http server")
+	certFile = flagenv.String("cert-file", "", "The certfile to establish a secure connection")
+	keyFile = flagenv.String("key-file", "", "The keyfile to establish a secure connection")
 )
 
 func main() {
@@ -33,34 +34,39 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	bot.Debug = true
+	webhookAddress := fmt.Sprintf(
+		"https://%s:%d/%s",
+		*webhookHost,
+		*webhookPort,
+		bot.Token,
+	)
 
-	_, err = bot.SetWebhook(tba.NewWebhook(fmt.Sprintf("%s:%d/%s", *webhookHost, *webhookPort, webhookPath)))
+	_, err = bot.SetWebhook(
+		tba.NewWebhookWithCert(
+			webhookAddress,
+			*certFile,
+		),
+	)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	info, err := bot.GetWebhookInfo()
-	if err != nil {
-		log.Fatal(err)
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	go http.ListenAndServeTLS("0.0.0.0:88", "cert.pem", "key.pem", nil)
+
+	for update := range updates {
+		log.Printf("%+v\n", update)
 	}
-
-	if info.LastErrorDate != 0 {
-		log.Printf("[Telegram callback failed]%s", info.LastErrorMessage)
-	}
-
-	n := negroni.Classic()
-	r := mux.NewRouter()
-	r.Methods("POST").Path("/v1/webhooks/telegram/").Name("TelegramWebhookHandler").HandlerFunc(handleTelegramWebhook)
-	n.UseHandler(r)
-
-	http.ListenAndServe("5.9.96.244:3030", n)
 }
 
 func handleTelegramWebhook(w http.ResponseWriter, r *http.Request) {
-	update, err := tba.GetWebhookUpdate(r)
+	log.Println(r)
+/*	update, err := tba.GetWebhookUpdate(r)
 	if err != nil {
 		w.Write([]byte("false"))
 	}
 
-	log.Println(update)
+	log.Println(update)*/
 }
